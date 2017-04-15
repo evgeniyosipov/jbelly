8617160de32bb6a672e9d3479d1268193c46171c
Evgeniy Osipov <osipoveg1990@gmail.com>
2017-04-15 11:55:51 +0300

    Added initial project files

---
+++ src/main/java/ru/evgeniyosipov/jbelly/JbellyApplication.java
@@ -0,0 +1,12 @@
+package ru.evgeniyosipov.jbelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbellyApplication.class, args);
+	}
+}
