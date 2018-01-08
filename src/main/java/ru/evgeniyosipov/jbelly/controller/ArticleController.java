8fe464c9ed507cc24548ab4cfbb5276766073e9e
Evgeniy Osipov <osipoveg1990@gmail.com>
2018-01-08 17:37:08 +0300

    Added creating articles functionality

--- src/main/java/ru/evgeniyosipov/jbelly/controller/ArticleController.java
+++ src/main/java/ru/evgeniyosipov/jbelly/controller/ArticleController.java
@@ -2,9 +2,16 @@ package ru.evgeniyosipov.jbelly.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.evgeniyosipov.jbelly.bindingModel.ArticleBindingModel;
+import ru.evgeniyosipov.jbelly.entity.Article;
+import ru.evgeniyosipov.jbelly.entity.User;
 import ru.evgeniyosipov.jbelly.repository.ArticleRepository;
 import ru.evgeniyosipov.jbelly.repository.UserRepository;
 
@@ -21,5 +28,24 @@ public class ArticleController {
         model.addAttribute("view", "article/create");
 
         return "base-layout";
+    }
+
+    @PostMapping("/article/create")
+    @PreAuthorize("isAuthenticated()")
+    public String createProcess (ArticleBindingModel articleBindingModel){
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+
+        User userEntity = this.userRepository.findByEmail(user.getUsername());
+
+        Article articleEntity = new Article(
+                articleBindingModel.getTitle(),
+                articleBindingModel.getContent(),
+                userEntity
+        );
+
+        this.articleRepository.saveAndFlush(articleEntity);
+
+        return "redirect:/";
     }
 }
