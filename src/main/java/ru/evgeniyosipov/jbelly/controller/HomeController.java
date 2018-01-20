f1d27bfa63b4d0cea4e4742038e971dd97447a44
Evgeniy Osipov <osipoveg1990@gmail.com>
2018-01-20 12:06:18 +0300

    Added access control

--- src/main/java/ru/evgeniyosipov/jbelly/controller/HomeController.java
+++ src/main/java/ru/evgeniyosipov/jbelly/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.evgeniyosipov.jbelly.entity.Article;
 import ru.evgeniyosipov.jbelly.repository.ArticleRepository;
 
@@ -23,6 +24,13 @@ public class HomeController {
 
         model.addAttribute("view", "home/index");
         model.addAttribute("articles", articles);
+
+        return "base-layout";
+    }
+
+    @RequestMapping("/error/403")
+    public String accessDenied(Model model){
+        model.addAttribute("view", "error/403");
 
         return "base-layout";
     }
