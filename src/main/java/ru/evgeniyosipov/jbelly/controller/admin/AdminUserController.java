ccff62900b9eab0476598b4e0872fa50e9e82ac0
Evgeniy Osipov <osipoveg1990@gmail.com>
2019-01-15 21:43:54 +0300

    Cosmetic changes

--- src/main/java/ru/evgeniyosipov/jbelly/controller/admin/AdminUserController.java
+++ src/main/java/ru/evgeniyosipov/jbelly/controller/admin/AdminUserController.java
@@ -1,6 +1,5 @@
 package ru.evgeniyosipov.jbelly.controller.admin;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
