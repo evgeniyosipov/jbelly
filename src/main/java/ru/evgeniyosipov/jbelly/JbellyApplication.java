b15adec15485b755e88034d1a4106b297456aad3
Evgeniy Osipov <osipoveg1990@gmail.com>
2019-12-12 19:22:15 +0300

    Fix environment variables check

--- src/main/java/ru/evgeniyosipov/jbelly/JbellyApplication.java
+++ src/main/java/ru/evgeniyosipov/jbelly/JbellyApplication.java
@@ -3,27 +3,24 @@ package ru.evgeniyosipov.jbelly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
 @SpringBootApplication
 public class JbellyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(JbellyApplication.class);
-		application.setDefaultProperties(checkEnv());
+		envInitCheck(application);
 		application.run(args);
 	}
 
-	private static Properties checkEnv(){
-		Properties properties = new Properties();
+	static void envInitCheck(SpringApplication application) {
 		String mysqlHost = System.getenv("JBELLY_MYSQL_HOST");
 		String mysqlPort = System.getenv("JBELLY_MYSQL_PORT");
+
 		if(mysqlHost == null || mysqlHost.isEmpty()){
-			properties.put("JBELLY_MYSQL_HOST", "localhost");
+			System.setProperty("JBELLY_MYSQL_HOST", "localhost");
 		}
 		if(mysqlPort == null || mysqlPort.isEmpty()){
-			properties.put("JBELLY_MYSQL_PORT", "3306");
+			System.setProperty("JBELLY_MYSQL_PORT", "3306");
 		}
-		return properties;
 	}
 }
---
+++ src/test/java/ru/evgeniyosipov/jbelly/config/TestContextInitializer.java
@@ -0,0 +1,20 @@
+package ru.evgeniyosipov.jbelly.config;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class TestContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+        String mysqlHost = System.getenv("JBELLY_MYSQL_HOST");
+        String mysqlPort = System.getenv("JBELLY_MYSQL_PORT");
+
+        if(mysqlHost == null || mysqlHost.isEmpty()){
+            System.setProperty("JBELLY_MYSQL_HOST", "localhost");
+        }
+        if(mysqlPort == null || mysqlPort.isEmpty()){
+            System.setProperty("JBELLY_MYSQL_PORT", "3306");
+        }
+    }
+}
