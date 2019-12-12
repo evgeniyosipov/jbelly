877cc0b6385370cf9ff926bc1432426827bdc178
Evgeniy Osipov <osipoveg1990@gmail.com>
2019-12-12 16:58:54 +0300

    Add environment variables for database connection

--- src/main/java/ru/evgeniyosipov/jbelly/JbellyApplication.java
+++ src/main/java/ru/evgeniyosipov/jbelly/JbellyApplication.java
@@ -3,10 +3,27 @@ package ru.evgeniyosipov.jbelly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class JbellyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JbellyApplication.class, args);
+		SpringApplication application = new SpringApplication(JbellyApplication.class);
+		application.setDefaultProperties(checkEnv());
+		application.run(args);
+	}
+
+	private static Properties checkEnv(){
+		Properties properties = new Properties();
+		String mysqlHost = System.getenv("JBELLY_MYSQL_HOST");
+		String mysqlPort = System.getenv("JBELLY_MYSQL_PORT");
+		if(mysqlHost == null || mysqlHost.isEmpty()){
+			properties.put("JBELLY_MYSQL_HOST", "localhost");
+		}
+		if(mysqlPort == null || mysqlPort.isEmpty()){
+			properties.put("JBELLY_MYSQL_PORT", "3306");
+		}
+		return properties;
 	}
 }
