a3165498eec922cddf773cf23374b9a3025396c0
Evgeniy Osipov <osipoveg1990@yandex.ru>
2023-07-25 16:20:33 +0300

    Update WebSecurityConfig.java (https://docs.spring.io/spring-security/reference/migration-7/configuration.html)

--- src/main/java/ru/evgeniyosipov/jbelly/config/WebSecurityConfig.java
+++ src/main/java/ru/evgeniyosipov/jbelly/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package ru.evgeniyosipov.jbelly.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,18 +15,22 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().permitAll()
-                .and()
-                .formLogin().loginPage("/login")
-                .usernameParameter("email").passwordParameter("password")
-                .and()
-                .logout().logoutSuccessUrl("/login?logout")
-                .and()
-                .exceptionHandling().accessDeniedPage("/error/403")
-                .and()
-                .csrf();
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .usernameParameter("email").passwordParameter("password")
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedPage("/error/403")
+                )
+                .csrf(Customizer.withDefaults());
         return http.build();
     }
 }
